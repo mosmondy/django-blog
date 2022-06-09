e9af39aa51d76ba30049709c6b79e0378dbe01dc
mosmondy <kai.moses17@gmail.com>
2022-06-09 09:28:05 +0300

    initial blog commit

---
+++ blog/views.py
@@ -0,0 +1,131 @@
+from multiprocessing import context
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Comment
+from django.core.paginator import Paginator, PageNotAnInteger,EmptyPage
+from django.views.generic import ListView
+from .forms import EmailPostForm, CommentForm, SearchForm
+from django.core.mail import send_mail
+from taggit.models import Tag
+from django.db.models import Count
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank, TrigramSimilarity
+
+
+# search posts
+def post_search(request):
+    form = SearchForm()
+    
+    query = None
+    results = []
+    
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            
+            search_vector = SearchVector('title','body')
+            search_query = SearchQuery(query)
+            
+            
+            
+            results = Post.published.annotate(
+                search = search_vector, rank=SearchRank(search_vector, search_query)
+            ).filter(search=search_query).order_by('-rank')
+            
+            # results = Post.published.annotate(
+            #     similarity  = TrigramSimilarity('title', query),
+            # ).filter(similarity__gt=0.1).order_by('-similarity')
+            
+            
+            
+    context = {'form':form, 'query':query,'results':results}
+        
+    return render (request, 'blog/post/search.html',context)
+
+# Create your views here.
+def post_share(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status='published')
+    
+    sent = False
+    if request.method ==  'POST':
+        form = EmailPostForm(request.POST)
+        sent = False
+        if form.is_valid():
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+
+            subject = f'{cd["name"]} recommends you read ' \
+                      f'{post.title}'
+            message = f'Read {post.title} at {post_url} \n\n' \
+                      f'{cd["name"]}\'s comments: {cd["comment"]}'
+            send_mail(subject, message, 'kai.moses17@gmail.com', [cd["to"]])
+            
+            sent = True
+            # return redirect('blog/post/post.html')
+            
+    else:
+        form = EmailPostForm()
+    
+    context = {'post':post, 'form':form,'sent':sent}
+    return render(request, 'blog/post/share.html', context)
+
+class PostList(ListView):
+    queryset = Post.published.all()
+    # model = Post || this allows django to build Post.objects.all() by default but here i wabt published posts hence we user our custom ORM
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
+
+# !function based view replaced above with class based view using ListView
+def post_list(request, tag_slug=None):
+    object_list = Post.published.all()
+    
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        object_list = object_list.filter(tags__in=[tag])
+    
+    paginator = Paginator(object_list, 3)
+    
+    page = request.GET.get('page')
+    
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+        
+    # print(posts)
+    context={'posts':posts, 'page':page,'tag':tag}
+    
+    
+ 
+
+    return render(request,'blog/post/list.html',context)
+
+def post_detail(request, year,month, day, post):
+       
+    post = get_object_or_404(Post, slug=post, status='published', publish__year=year, publish__month=month, publish__day=day)
+    
+    comments = post.comments.filter(active=True)
+    new_comment = None
+    
+    
+    if request.method == 'POST':
+        comment_form = CommentForm(request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.post = post
+            new_comment.save()
+            
+    else:
+        comment_form = CommentForm()
+            
+    
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags','-publish')[:4]
+    
+    context = {'post':post,'comments':comments,'comment_form':comment_form,'new_comment':new_comment,'similar_posts':similar_posts}
+    return render(request, 'blog/post/detail.html',context)
